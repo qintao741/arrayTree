7630904671f6388dad8042afc84eb5d0c44df9f1
刘滨涛 <liubintao@meituan.com>
2017-03-31 20:49:20 +0800

    add

--- test.js
+++ test.js
@@ -4,126 +4,53 @@
 var array = [
     {
         "id": 1,
-        "parentId": 0,
-        "name": "王兴(总办)",
-        "settingId": 625,
-        "employeeId": "000001",
-        "level": 1,
-        "isClicked": null,
-        "leaf": false
+        "parent": 0
     },
     {
         "id": 2,
-        "parentId": 1,
-        "name": "穆荣均(总办)",
-        "settingId": 625,
-        "employeeId": "000002",
-        "level": 2,
-        "isClicked": null,
-        "leaf": false,
-        "isSubmit": true,
+        "parent": 1
     },
     {
         "id": 3,
-        "parentId": 2,
-        "name": "邓昀泽(企业平台研发部)",
-        "settingId": 625,
-        "employeeId": "028103",
-        "level": 3,
-        "isClicked": null,
-        "leaf": false
+        "parent": 2
     },
     {
         "id": 4,
-        "parentId": 3,
-        "name": "方正柱(企业平台研发部)",
-        "settingId": 625,
-        "employeeId": "039984",
-        "level": 4,
-        "isClicked": null,
-        "leaf": false
+        "parent": 3
     },
     {
         "id": 5,
-        "parentId": 4,
-        "name": "马杰星(HR研发组)",
-        "settingId": 625,
-        "employeeId": "0002410",
-        "level": 5,
-        "isClicked": null,
-        "leaf": true
+        "parent": 4
     },
     {
         "id": 6,
-        "parentId": 4,
-        "name": "喻峙元(HR平台组)",
-        "settingId": 625,
-        "employeeId": "03015798",
-        "level": 5,
-        "isClicked": null,
-        "leaf": true
+        "parent": 4
     }];
+
 var tree = [
     {
         "id": 1,
-        "parentId": 0,
-        "name": "王兴(总办)",
-        "settingId": 625,
-        "employeeId": "000001",
-        "level": 1,
-        "isClicked": null,
-        "leaf": false,
-        "childrenLits": [
+        "parent": 0,
+        "list": [
             {
                 "id": 2,
-                "parentId": 1,
-                "name": "穆荣均(总办)",
-                "settingId": 625,
-                "employeeId": "000002",
-                "level": 2,
-                "isClicked": null,
-                "leaf": false,
-                "isSubmit": true,
-                "childrenLits": [
+                "parent": 1,
+                "list": [
                     {
                         "id": 3,
-                        "parentId": 2,
-                        "name": "邓昀泽(企业平台研发部)",
-                        "settingId": 625,
-                        "employeeId": "028103",
-                        "level": 3,
-                        "isClicked": null,
-                        "leaf": false,
-                        "childrenLits": [
+                        "parent": 2,
+                        "list": [
                             {
                                 "id": 4,
-                                "parentId": 3,
-                                "name": "方正柱(企业平台研发部)",
-                                "settingId": 625,
-                                "employeeId": "039984",
-                                "level": 4,
-                                "isClicked": null,
-                                "leaf": false,
-                                "childrenLits": [
+                                "parent": 3,
+                                "list": [
                                     {
                                         "id": 5,
-                                        "parentId": 4,
-                                        "name": "马杰星(HR研发组)",
-                                        "settingId": 625,
-                                        "employeeId": "0002410",
-                                        "level": 5,
-                                        "isClicked": null,
-                                        "leaf": true
+                                        "parent": 4
                                     },
                                     {
                                         "id": 6,
-                                        "parentId": 4,
-                                        "name": "喻峙元(HR平台组)",
-                                        "settingId": 625,
-                                        "employeeId": "03015798",
-                                        "level": 5,
-                                        "isClicked": null,
-                                        "leaf": true
+                                        "parent": 4
                                     }
                                 ]
                             }
@@ -133,12 +60,18 @@ var tree = [
             }
         ]
     }
-]
+];
 
-var util = require("./index.js")
+var util = require("./index.js");
+
+var d = util.convertArrayToTree(array,{id:"id",parentId:"parent",childrenList:"list"});
+
+var z = util.convertTreeToArray(tree,{childrenList:"list",needDelete:true});
+
+console.log(JSON.stringify(z));
 
 
-var d = util.convertArrayToTree(array);
-var d2 = util.convertArrayToTree(array);
-console.log(JSON.stringify(d));
-console.log(JSON.stringify(d2));
+
+
+// var d2 = util.convertTreeToArray(tree);
+
